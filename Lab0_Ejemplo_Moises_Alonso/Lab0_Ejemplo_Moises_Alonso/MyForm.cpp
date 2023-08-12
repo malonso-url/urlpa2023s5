3e18c7c7b6c5767f806505624cde789539f373dd
Moises Alonso <maalonso@uvg.edu.gt>
2023-08-12 08:47:01 -0600

    Subir primer ejemplo de uso Cpp / CLI

---
+++ Lab0_Ejemplo_Moises_Alonso/Lab0_Ejemplo_Moises_Alonso/MyForm.cpp
@@ -0,0 +1,12 @@
+#include "MyForm.h"
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThreadAttribute]
+int main(array<String^>^ args) {
+	Application::EnableVisualStyles;
+	Application::SetCompatibleTextRenderingDefault(false);
+	Lab0EjemploMoisesAlonso::MyForm mainForm;
+	Application::Run(%mainForm);
+	return 0;
+}

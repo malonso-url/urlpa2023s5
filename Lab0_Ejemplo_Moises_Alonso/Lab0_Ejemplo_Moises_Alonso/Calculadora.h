520b7af45aa8fad1fab01372db9df3bee5e8812b
Moises Alonso <maalonso@uvg.edu.gt>
2023-08-19 00:38:32 -0600

    Agregar ref class calculadora, no se ha utilizado aún

---
+++ Lab0_Ejemplo_Moises_Alonso/Lab0_Ejemplo_Moises_Alonso/Calculadora.h
@@ -0,0 +1,18 @@
+#pragma once
+ref class Calculadora
+{
+public: 
+	int suma(int sumando1, int sumando2);
+
+	int resta(int minuendo, int sustraendo);
+
+	int multiplicacion(int factor1, int factor2);
+
+	int cociente(int dividendo, int divisor);
+
+	int residuo(int dividendo, int divisor);
+
+	int potencia(int base, int exponente);
+
+};
+
